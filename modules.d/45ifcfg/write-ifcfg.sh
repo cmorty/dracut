cmorty/dracut

commit 32ec0a762d1dce36f20857ffd222863a3d550ed7

ifcfg/write-ifcfg.sh: add "IPV6INIT=yes" for IPv6
https://bugzilla.redhat.com/show_bug.cgi?id=852808

modules.d/45ifcfg/write-ifcfg.sh
@@ -105,6 +105,7 @@ for netif in $IFACES ; do
         [ -n "$mtu" ] && echo "MTU=$mtu"
         if [ -f /tmp/net.$netif.lease ]; then
             strstr "$ip" '*:*:*' &&
+            echo "IPV6INIT=yes"
             echo "DHCPV6C=yes"
             echo "BOOTPROTO=dhcp"
             cp /tmp/net.$netif.lease /tmp/ifcfg-leases/dhclient-$uuid-$netif.lease
@@ -112,6 +113,7 @@ for netif in $IFACES ; do
             # If we've booted with static ip= lines, the override file is there
             [ -e /tmp/net.$netif.override ] && . /tmp/net.$netif.override
             if strstr "$ip" '*:*:*'; then
+                echo "IPV6INIT=yes"
                 echo "IPV6_AUTOCONF=no"
                 echo "IPV6ADDR=$ip/$mask"
             else

commit 169f4fa838c7c1ff5884690ab75c17dcbd75e068

Don't write DHCPV6C=yes for each dhcp (ipv4) configuration.
Caused probably by commit 32ec0a762d1dce36f20857ffd222863a3d550ed7

modules.d/45ifcfg/write-ifcfg.sh
@@ -105,9 +105,7 @@ for netif in $IFACES ; do
         echo "UUID=$uuid"
         [ -n "$mtu" ] && echo "MTU=$mtu"
         if [ -f /tmp/net.$netif.lease ]; then
-            strstr "$ip" '*:*:*' &&
-            echo "IPV6INIT=yes"
-            echo "DHCPV6C=yes"
+            strstr "$ip" '*:*:*' && echo "IPV6INIT=yes"
             echo "BOOTPROTO=dhcp"
             cp /tmp/net.$netif.lease /tmp/ifcfg-leases/dhclient-$uuid-$netif.lease
         else